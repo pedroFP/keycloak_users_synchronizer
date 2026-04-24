27a3c056c1a77dd95d4d3be8c1659b5165b5f27e
pedro <pedro.araya.labra@protonmail.com>
2026-04-24 17:19:32 +0000

    require activemodel and util file in main

--- keycloak.rb
+++ keycloak.rb
@@ -3,10 +3,15 @@ require 'json'
 require 'net/http'
 require 'openssl'
 require 'dotenv/load'
+require 'active_model'
+
+require_relative 'util/model_serializer'
 
 module Keycloak
   BASE_URL = ENV.fetch('KEYCLOAK_BASE_URL', 'http://localhost:8080').freeze
   REALM = ENV.fetch('KEYCLOAK_REALM').freeze
 end
 
+
 Dir[File.join(__dir__, 'app', '**', '*.rb')].each { |f| require_relative f }
+
