bab558b41bd95830c3122b2349c4b6b021faa490
pedro <pedro.araya.labra@protonmail.com>
2026-04-27 14:24:47 +0000

    refactor SyncKeycloakUsers

--- app/service/sync_keycloak_users.rb
+++ app/service/sync_keycloak_users.rb
@@ -1,12 +1,15 @@
+# Service in charge of producing a kafka message for each Keycloak user
+
 class SyncKeycloakUsers
+  USERS_PER_PAGE = 100
+
   def self.call
     users_count = Keycloak::User.count
-    users_per_page = 100
-    number_of_pages = (users_count.to_f / users_per_page).ceil # round up
+    number_of_pages = (users_count.to_f / USERS_PER_PAGE).ceil
     page = 0
 
-    while page <= (number_of_pages - 1)
-      Keycloak::User.page(page).limit(users_per_page).each do |user|
+    while page < number_of_pages
+      Keycloak::User.page(page).limit(USERS_PER_PAGE).each do |user|
         UserProducer.new(user).produce
       end
       page += 1
